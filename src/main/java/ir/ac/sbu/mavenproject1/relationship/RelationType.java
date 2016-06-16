a1e01fbf487fb06ccff5b8fc276e2fcc5329bd77
Shirin Tahmasebi <tahmasebi@arsh.co>
2016-06-17 03:42:59 +0430

    Neo4j basics added

---
+++ src/main/java/ir/ac/sbu/mavenproject1/relationship/RelationType.java
@@ -0,0 +1,8 @@
+package ir.ac.sbu.mavenproject1.relationship;
+
+import org.neo4j.graphdb.RelationshipType;
+
+public enum RelationType implements RelationshipType {
+
+    Rate, See;
+}
--- src/main/java/ir/ac/sbu/mavenproject1/util/Neo4jHelper.java
+++ src/main/java/ir/ac/sbu/mavenproject1/util/Neo4jHelper.java
@@ -1,14 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ir.ac.sbu.mavenproject1.util;
 
-/**
- *
- * @author Shirin
- */
+import ir.ac.sbu.mavenproject1.node.NodeType;
+import java.util.Iterator;
+import org.neo4j.cypher.ExecutionEngine;
+import org.neo4j.cypher.ExecutionResult;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.kernel.impl.util.StringLogger;
+
 public class Neo4jHelper {
-    
+
+    public static GraphDatabaseService getGraphDb() {
+        GraphDatabaseFactory dbfactoryy = new GraphDatabaseFactory();
+        GraphDatabaseService graphdb = dbfactoryy.newEmbeddedDatabase("//Users//apple//Documents//Neo4j//default.graphdb");
+        return graphdb;
+    }
+
+    public static Transaction createTransation() {
+        GraphDatabaseService graphdb = getGraphDb();
+        Transaction tx = graphdb.beginTx();
+        return tx;
+    }
+
+    public static void commitTransaction(Transaction tx) {
+        tx.success();
+    }
+
+    public static Node createNode(NodeType type) {
+        return getGraphDb().createNode(type);
+    }
+
+    public static void setProperty(Node node, String propertyName, String propertyValue) {
+        node.setProperty(propertyName, propertyValue);
+    }
+
+    public static void setProperty(Relationship relationship, String propertyName, String propertyValue) {
+        relationship.setProperty(propertyName, propertyValue);
+    }
+
+    public static Relationship createRelationship(Node fromNode, Node toNode, RelationshipType type) {
+        return fromNode.createRelationshipTo(toNode, type);
+    }
+
+    public static Iterator getQueryResult(String query) {
+        ExecutionEngine engine = new ExecutionEngine(getGraphDb(), StringLogger.SYSTEM);
+        ExecutionResult result;
+        result = engine.execute(query);
+        System.out.println("getQueryResult " + result.dumpToString());
+        Iterator itr = result.javaIterator();
+        return itr;
+    }
 }
